def19247027135b281baec64775a3899222e7a39
Vlad356 <vladstadniychuk@gmail.com>
2023-10-12 20:21:26 +0300

    Step1

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -1,2 +1,20 @@
 public class StringCalculator {
+    public static int add(String numbers)
+    {
+        int number_sum = 0;
+        if (numbers.isEmpty())
+        {
+            return 0;
+        }
+        String[] ArrayOfNums = numbers.split(",");
+        for (int i = 0; i < ArrayOfNums.length; i++)
+        {
+            number_sum += Integer.parseInt(ArrayOfNums[i]);
+        }
+        if (ArrayOfNums.length > 2)
+        {
+            throw new Error("Inputted more than 2 numbers!");
+        }
+        return number_sum;
+    }
 }
--- lab1/src/test/java/CalculatorTest.java
+++ lab1/src/test/java/CalculatorTest.java
@@ -1,2 +1,24 @@
-public class CalculatorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest
+{
+    @Test
+    void ShouldReturnZero()
+    {
+        assertEquals(0,StringCalculator.add(""));
+    }
+    @Test
+    void ShouldReturnOne()
+    {
+        assertEquals(1,StringCalculator.add("1"));
+    }
+    @Test
+    void ShouldReturnThree()
+    {
+        assertEquals(3,StringCalculator.add("1,2"));
+    }
+
 }
